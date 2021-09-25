8ec5dadef597963f1318eac2bfbb8da1a16a17d3
Sergio Victor <sergiovmml@gmail.com>
2021-09-25 19:18:57 -0300

    1

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,15 +1,64 @@
-import React from 'react'
+import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
+import { 
+    Container, 
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGretting,
+    UserName,
+    Icon,
+    HighlightCards,
+    Transactions,
+    Title
+ } from './styles'
 
-
-import { Container, Title } from './styles'
-
-
-export function Dashboard() {
-    return (
-        <Container>
-            <Title>
-                DashBoard
-            </Title>
-        </Container>
-    )
+export function Dashboard(){
+  return (
+    <Container>
+        <Header>
+            <UserWrapper>
+            <UserInfo>
+                <Photo 
+                
+                source={{ uri: 'https://avatars.githubusercontent.com/u/20932056?v=4'}}/>
+                <User>
+                    <UserGretting>  Olá,</UserGretting>
+                      <UserName>Sergio</UserName>
+                </User>           
+            </UserInfo>
+            <Icon name="power"/>
+            </UserWrapper>
+        </Header>
+    <HighlightCards >
+     <HighlightCard
+          type="up" 
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril" 
+          />
+     <HighlightCard
+     type="down" 
+     title="Saídas"
+     amount="R$ 1.259,00"
+     lastTransaction="Última saída dia 13 de abril"
+     />  
+     <HighlightCard 
+     type="total"
+     title="Total"
+     amount="R$ 16.141,00"
+     lastTransaction="01 à 16 de abril"
+     />  
+     </HighlightCards>   
+    
+    <Transactions>
+        <Title>
+            Listagem
+        </Title>
+    </Transactions>
+    
+    </Container>
+  )
 }
