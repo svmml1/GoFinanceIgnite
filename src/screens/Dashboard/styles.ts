cc05965783887c46069bfb9e547767ca5172dede
Sergio Victor <sergiovmml@gmail.com>
2021-09-25 14:16:56 -0300

    1

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native'
+
+
+
+export const Container = styled.View `
+    flex: 1;
+    background-color: #cd3442;
+    align-items: center;
+    justify-content: center;
+`; 
+
+export const Title = styled.Text`
+font-size: 24px;
+font-weight: bold;
+
+`;
