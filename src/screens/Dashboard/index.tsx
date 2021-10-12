7e998cb1985bb8f54d4a0f2ba391010064e0eee4
Sergio Victor <sergiovmml@gmail.com>
2021-10-12 11:17:32 -0300

    1

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -22,6 +22,7 @@ import {
   LogoutButton,
   LoadContainer
 } from './styles'
+import { useAuth } from '../../hooks/auth';
 
 export interface DataListProps extends TransactionCardPros {
   id: string
@@ -44,6 +45,7 @@ export function Dashboard() {
   const [highlightData, setHighlightData] = useState<HighlightData>({} as HighlightData);
 
   const theme = useTheme();
+  const { signOut } = useAuth()
 
   function getLastTransactionDate(
     collection: DataListProps[],
@@ -168,7 +170,7 @@ export function Dashboard() {
                     <UserName>Sergio</UserName>
                   </User>
                 </UserInfo>
-                <LogoutButton onPress={() => { }}>
+                <LogoutButton onPress={signOut}>
 
                   <Icon name="power" />
                 </LogoutButton>
